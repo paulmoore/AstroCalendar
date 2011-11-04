13b218b14b78e77c1b3ee81a0151d31c7c6bb225
Stephen Smithbower <smithy.@gmail.com>
2011-11-04 12:38:44 -0700

    Added central data management classes to the project. All data acquisition/storage should be done through them.

---
+++ AstroCalendar/DayContainer.m
@@ -0,0 +1,22 @@
+//
+//  DayContainer.m
+//  HTTPRequestTest
+//
+//  Created by Stephen Smithbower on 11-11-01.
+//  Copyright (c) 2011 University of British Columbia. All rights reserved.
+//
+
+#import "DayContainer.h"
+
+@implementation DayContainer
+
+	@synthesize date;
+    @synthesize sunrise;
+    @synthesize sunset;
+    @synthesize moonrise;
+    @synthesize moonset;
+    @synthesize tithi;
+    @synthesize fortnight;
+    @synthesize lunarMonth;
+
+@end
---
+++ AstroCalendar/MasterDataHandler.h
@@ -0,0 +1,58 @@
+//
+//  MasterDataHandler.h
+//  HTTPRequestTest
+//
+//  Created by Stephen Smithbower on 11-11-01.
+//  Copyright (c) 2011 University of British Columbia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AFNetworking.h"
+#import "AFJSONRequestOperation.h"
+#import "DayContainer.h"
+
+@interface MasterDataHandler : NSObject
+
+	//////////////////////////////////////////////////////////////
+    // Properties												//
+    //////////////////////////////////////////////////////////////
+    
+    // Target URL that API GET requests are sent to. Parameters will
+    // be tagged on to the end of this before converting to an NSUrl.
+	@property(copy) NSString *apiEndpoint;
+
+
+
+	
+    //////////////////////////////////////////////////////////////
+    // Class Methods											//
+    //////////////////////////////////////////////////////////////
+    
+    //Returns an array of DayContainer objects with their populated
+    //data.
+    //
+    //startDate: The (inclusive) start date to begin polling information.
+    //endDate: The (inclusive) end date to stop polling information.
+	//-(NSArray*)getDateRange:(NSDate*)startDate: (NSDate*)endDate;
+
+
+
+
+	
+    //////////////////////////////////////////////////////////////
+    // Instance Methods											//
+    //////////////////////////////////////////////////////////////
+    
+    //Asynchronously asks the API for missing data. Fills in the
+    //data cache, and sets up a delegate to return the results
+    //as necessary.
+	-(void)askApiForDates:(NSDate*) startDate: (NSDate*) endDate;
+	
+    -(NSArray*)parseJSONDateRange: (id)json;
+    
+    -(void)registerAlertOnDate: (NSDate*) date: (NSString*) message;
+
+    -(void)saveSettings;
+    -(void)loadSettings;
+    
+@end
