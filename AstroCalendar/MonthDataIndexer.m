1aa39b544ee1d4536cd0daef91f284242acc9301
Stephen Smithbower <smithy.@gmail.com>
2011-11-17 14:31:57 -0800

    Added code to pull from legit API endpoint, read in and write settings (plist), a ring buffer, caching behaviour for date data

---
+++ AstroCalendar/MonthDataIndexer.m
@@ -0,0 +1,19 @@
+//
+//  MonthDataIndexer.m
+//  HTTPRequestTest
+//
+//  Created by Stephen Smithbower on 11-11-14.
+//  Copyright (c) 2011 University of British Columbia. All rights reserved.
+//
+
+#import "MonthDataIndexer.h"
+
+@implementation MonthDataIndexer
+
+	//Auto-properties.
+    @synthesize date;
+    @synthesize cachedDate;
+    @synthesize index;
+    @synthesize dayCount;
+
+@end
