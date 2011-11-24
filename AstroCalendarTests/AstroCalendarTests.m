527f7ae22ef5fb67a6097029687645464b2359a0
Stephen Smithbower <smithy.@gmail.com>
2011-11-23 16:36:12 -0800

    ADDED Unit tests for date data caching (in-memory cache, read/write to device for permanent storage).

--- AstroCalendarTests/AstroCalendarTests.m
+++ AstroCalendarTests/AstroCalendarTests.m
@@ -30,6 +30,7 @@
 
 #import "AstroCalendarTests.h"
 #import "RingBuffer.h"
+#import "MasterDataHandler.h"
 
 @implementation AstroCalendarTests
 
@@ -140,5 +141,236 @@
     }
 }
 
+//////////////////////////////////////////////////////////
+// DAY DATA CACHING TESTS								//
+//////////////////////////////////////////////////////////
+-(void) testCacheAddDay
+{
+	//Helps us out for conversion to NSDates.
+    NSCalendar *helperCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+
+	MasterDataHandler *dataHandler = [[MasterDataHandler alloc]init];
+    [dataHandler clearCache];
+    
+    //Create dummy day.
+    DayContainer *dummyDay = [[DayContainer alloc]init];
+
+    //Fill the dummyDay with dummy info - the only thing that matters for our tests is the month/day/year.
+    NSDateComponents *dummyBuildDate = [[NSDateComponents alloc] init];
+    [dummyBuildDate setDay:14];
+	[dummyBuildDate setMonth:3];
+    [dummyBuildDate setYear:1989]; //Stephen's birthday!
+        
+    dummyDay.date = [helperCalendar dateFromComponents:dummyBuildDate];
+    
+    //dummyDay.sunrise = [helperFormatter dateFromString:@"01-01-200001-01-01"];
+    //dummyDay.sunset = [helperFormatter dateFromString:@"01-01-200001-01-01"];
+    //dummyDay.moonrise = [helperFormatter dateFromString:@"01-01-200001-01-01"];
+   	//dummyDay.moonset = [helperFormatter dateFromString:@"01-01-200001-01-01"];
+        
+    //dummyDay.tithi = @"it's a tithi!";
+    //dummyDay.fortnight = @"it's a fortnight!";
+    //dummyDay.lunarMonth = @"it's a lunar month!";
+    
+    
+    //Now, add the day!
+    [dataHandler addDayToCache:dummyDay];
+    
+    //Now, let's see if it's in the ring buffer.
+    NSArray *indexBufferElement = [[MasterDataHandler getDataCacheIndexer] elements];
+    
+    STAssertTrue([[MasterDataHandler getDataCacheIndexer] count] == 1 , [NSString stringWithFormat: @"Actual value: %i", [[MasterDataHandler getDataCacheIndexer] count]]);
+    
+    NSDate *indexedDate = [[indexBufferElement objectAtIndex:0] objectForKey:@"date"]; //((DayContainer*)[indexBufferElement objectAtIndex:0]).date;
+    
+    STAssertTrue([indexedDate isEqual: dummyDay.date], @"Failed to store an index to the month containing the dummy day in the cache index buffer. Expected: %@, Actual: %@", dummyDay.date, indexedDate);
+    
+    //Now check to see if it's been stored in the appropriate dictionary.
+    NSDictionary *monthSet = (NSDictionary*)[[MasterDataHandler getDataCache] objectForKey:[[NSNumber numberWithInt:0] description]];
+    
+    indexedDate = ((DayContainer*)[monthSet objectForKey:[[NSNumber numberWithInt:14] description]]).date;
+    
+    STAssertTrue([indexedDate isEqual: dummyDay.date], [NSString stringWithFormat:@"Failed to store the actual data information in the in-memory cache dictionary. Expected: %@, Actual: %@", dummyDay.date, indexedDate]);
+    
+    
+    //Clear out previous test. Need to start fresh to make sure our assumptions about indices hold.
+    [dataHandler clearCache];
+    
+    STAssertTrue([[MasterDataHandler getDataCacheIndexer] count] == 0, @"The cache should be empty, but it is not");
+    
+    //Add a day for every month (24 of 'em)!
+    [dummyBuildDate setDay:2];
+    for (int i = 1; i <= 24; i++)
+    {
+    	[dummyBuildDate setMonth:i];
+        
+        DayContainer *tDate = [[DayContainer alloc]init];
+        tDate.date = [helperCalendar dateFromComponents:dummyBuildDate];
+        int newIndex = [dataHandler addDayToCache:tDate];
+        
+        STAssertTrue([[MasterDataHandler getDataCacheIndexer] count] == i, [NSString stringWithFormat: @"Indexer size was wrong. Expected: %i, Actual: %i", i, [[MasterDataHandler getDataCacheIndexer] count]]);
+        
+        STAssertTrue(newIndex == (i - 1), [NSString stringWithFormat: @"Index of new month is wrong. Expected: %i, Actual: %i", (i - 1), newIndex]);
+        
+        STAssertTrue([[MasterDataHandler getDataCache] count] == i, [NSString stringWithFormat:@"In-memory dictionary does not have the corrent number of months. Expected: %i, Actual: %i", i, [[MasterDataHandler getDataCache] count]]);
+    }
+    
+    //Now, check to make sure they're in there.
+    STAssertTrue([[MasterDataHandler getDataCacheIndexer] count] == 24, @"Cache indexer element count wrong. Expected: 24, Actual: %i", [[MasterDataHandler getDataCacheIndexer] count]);
+    
+    //Ok, let's check the date on every element.
+    for (int i = 1; i <= 24; i++)
+    {
+    	NSDictionary *cachedMonthSet = (NSDictionary*)[[MasterDataHandler getDataCache] objectForKey:[[NSNumber numberWithInt:(i-1)] description]];
+        
+        STAssertFalse(cachedMonthSet == nil, [NSString stringWithFormat: @"The month set for month %i could not be retrieved.", i - 1]);
+        
+        DayContainer *cachedDay = (DayContainer*)[cachedMonthSet objectForKey:[[NSNumber numberWithInt:2] description]];
+        
+        STAssertFalse(cachedDay == nil, @"The day could not be retrieved");
+        
+        //Verify the dates.
+        NSDateComponents *cachedDayComponents = [helperCalendar components:NSDayCalendarUnit | NSMonthCalendarUnit  fromDate:cachedDay.date];
+        
+        STAssertTrue([cachedDayComponents day] == 2, [NSString stringWithFormat: @"Retrieved month is wrong. Expected: %i, Actual: %i", i, [cachedDayComponents day]]);
+    }
+    
+    //Cleanup!
+    [dataHandler clearCache];
+}
+
+-(void) testCacheRetrieveDayFromCache
+{
+	MasterDataHandler *dataHandler = [[MasterDataHandler alloc]init];
+    
+    //Helps us out for conversion to NSDates.
+    NSCalendar *helperCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+
+    
+    //Add one day, and pull it back out.
+    DayContainer *dummyDay = [[DayContainer alloc]init];
+    
+    //Fill the dummyDay with dummy info - the only thing that matters for our tests is the month/day/year.
+    NSDateComponents *dummyBuildDate = [[NSDateComponents alloc] init];
+    [dummyBuildDate setDay:14];
+	[dummyBuildDate setMonth:3];
+    [dummyBuildDate setYear:1989]; //Stephen's birthday!
+        
+    dummyDay.date = [helperCalendar dateFromComponents:dummyBuildDate];
+    
+    //Now, add the day!
+    [dataHandler addDayToCache:dummyDay];
+    
+	STAssertTrue([[dataHandler retrieveDayFromCache: dummyDay.date] isEqual: dummyDay], @"Could not retrieve the dummy day inserted into the cache");
+    
+    //Try inserting a bunch of days and pull them back.
+    
+    NSMutableArray *dummyDays = [[NSMutableArray alloc]init];
+    
+    [dummyBuildDate setMonth:4];
+    for (int i = 1; i < 24; i++)
+    {
+    	[dummyBuildDate setDay:i];
+        
+        DayContainer *testDay = [[DayContainer alloc]init];
+        testDay.date = [helperCalendar dateFromComponents:dummyBuildDate];
+        
+        [dataHandler addDayToCache:testDay];
+        [dummyDays addObject:testDay];
+    }
+    
+    //Now, check that we can pull them all back out.
+    for (DayContainer *day in dummyDays)
+	{
+    	DayContainer *tmpDay = (DayContainer*)[dataHandler retrieveDayFromCache:day.date];
+    
+    	STAssertTrue([tmpDay.date isEqual: day.date], [NSString stringWithFormat:@"Days do not match. Expected: %@, Actual: %@", day.date, tmpDay.date]);
+	}
+}
+
+-(void) testCacheWriteLoad
+{
+	MasterDataHandler *dataHandler = [[MasterDataHandler alloc]init];
+    [dataHandler clearCache];
+    
+    //Helps us out for conversion to NSDates.
+    NSCalendar *helperCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+
+    //Create a bunch of days to fill a month.
+    NSMutableArray *testDaySet = [[NSMutableArray alloc]init];
+    
+    NSDateComponents *dummyBuildDate = [[NSDateComponents alloc] init];
+    [dummyBuildDate setMonth:3];
+    [dummyBuildDate setYear:1989]; //Stephen's birthday!
+
+    
+    for (int i = 1; i < 24; i += 2)
+    {
+    	DayContainer *dummyDay = [[DayContainer alloc]init];
+    
+    	//Fill the dummyDay with dummy info - the only thing that matters for our tests is the month/day/year.
+        [dummyBuildDate setDay: i];
+    	dummyDay.date = [helperCalendar dateFromComponents:dummyBuildDate];
+        
+        [dataHandler addDayToCache:dummyDay];
+        [testDaySet addObject:dummyDay];
+    }
+    
+    //Save it out to disk.
+    [dataHandler writeCache:((DayContainer*)[testDaySet objectAtIndex:0]).date];
+    
+    //Load the cache back in.
+	[dataHandler loadCache];
+    
+    //Now, verify dates.
+    for (DayContainer *day in testDaySet)
+	{
+    	DayContainer *tmpDay = (DayContainer*)[dataHandler retrieveDayFromCache:day.date];
+    
+    	STAssertTrue([tmpDay.date isEqual: day.date], [NSString stringWithFormat:@"Days do not match. Expected: %@, Actual: %@", day.date, tmpDay.date]);
+	}
+
+}
+
+
+//////////////////////////////////////////////////////////
+// DAYCONTAINER TESTS									//
+//////////////////////////////////////////////////////////
+-(void)testDayContainerEncodeDecode
+{
+    //Helps us out for conversion to NSDates.
+    NSCalendar *helperCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+
+
+ 	//Create dummy day.
+    DayContainer *dummyDay = [[DayContainer alloc]init];
+
+    //Fill the dummyDay with dummy info - the only thing that matters for our tests is the month/day/year.
+    NSDateComponents *dummyBuildDate = [[NSDateComponents alloc] init];
+    [dummyBuildDate setDay:14];
+	[dummyBuildDate setMonth:3];
+    [dummyBuildDate setYear:1989]; //Stephen's birthday!
+        
+    dummyDay.date = [helperCalendar dateFromComponents:dummyBuildDate];
+    
+    NSDateFormatter *helperFormatter = [[NSDateFormatter alloc] init];
+    [helperFormatter setDateFormat:@"dd-MM-yyyyhh-mm-ss"];
+    
+    dummyDay.sunrise = [helperFormatter dateFromString:@"01-01-200001-01-01"];
+    dummyDay.sunset = [helperFormatter dateFromString:@"01-01-200001-01-01"];
+    dummyDay.moonrise = [helperFormatter dateFromString:@"01-01-200001-01-01"];
+   	dummyDay.moonset = [helperFormatter dateFromString:@"01-01-200001-01-01"];
+        
+    dummyDay.tithi = @"it's a tithi!";
+    dummyDay.fortnight = @"it's a fortnight!";
+    dummyDay.lunarMonth = @"it's a lunar month!";
+    
+    NSDictionary *encoded = [dummyDay encodeAsDictionary];
+    
+    DayContainer *decoded = [[DayContainer alloc]init];
+    [decoded decodeFromDictionary:encoded];
+    
+    STAssertTrue([dummyDay.tithi compare: decoded.tithi] == NSOrderedSame, [NSString stringWithFormat:@"Tithis do not match. Expected: %@, Actual: %@", dummyDay.tithi, decoded.tithi]);
+}
 
 @end
